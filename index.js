c7f112e9688bc5ab2b23a354a46f61b34a25e86f
david <davidf1212@gmail.com>
2015-01-31 01:13:27 -0500

    refactor, css folder

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname+'/public'));
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
 });
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,11 @@
 var socket = io();
+
 $('form').submit(function(){
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
+	socket.emit('chat message', $('#m').val());
+	$('#m').val('');
+	return false;
 });
+
 socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
+	$('#messages').append($('<li>').text(msg));
 });
